3def457de911241cdb1bd678a443c45ebb5f4c1a
Markian Tsybran <mtsybran@nexway.com>
2020-09-23 00:29:22 +0300

    refactor

--- src/screens/MainScreen/MainScreen.jsx
+++ src/screens/MainScreen/MainScreen.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-import localization from '../../localization';
-
-import SideBar from './../../components/SideBar';
 
 const MainScreen = () => (
-  <SideBar/>
+  <Typography>Main</Typography>
 );
 
 export default MainScreen;
