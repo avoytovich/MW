fdb679873b0acf30ba809add12c9499e79dcf590
Markian Tsybran <mtsybran@nexway.com>
2020-09-23 01:22:57 +0300

    refactor/main-layout

--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
 import PropTypes from 'prop-types';
 import SideBar from '../../components/SideBar';
 
 const MainLayout = ({ children }) => (
-  <>
-    <SideBar />
-    {children}
-  </>
+  <Grid container>
+    <Grid item><SideBar /></Grid>
+
+    <Grid container item style={{ paddingLeft: 240 }}>
+      <Grid item>{children}</Grid>
+    </Grid>
+  </Grid>
 );
 
 MainLayout.propTypes = {
