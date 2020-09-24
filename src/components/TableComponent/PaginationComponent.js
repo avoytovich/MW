5b0af5be32be050e3bba729a85faee2fa2341295
Lyudmyla Pomohaibo <lyudmyla.pomohaibo@soft-innovation.ch>
2020-09-24 11:22:47 +0300

    feature/IAP-8768;Make pagination layout;

--- src/components/TableComponent/PaginationComponent.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-
-const PaginationComponent = ({totalPages}) => {
-  const [totalPages, setTotalPages] = useState();
-
-  return <div>PaginationComponent</div>;
-};
-
-export default PaginationComponent;
--- src/screens/ProductsScreen/ProductsScreen.jsx
+++ src/screens/ProductsScreen/ProductsScreen.jsx
@@ -4,15 +4,22 @@ import api from '../../api';
 
 const ProductsScreen = () => {
   const [products, setProducts] = useState({});
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     api.getProducts().then((res) => {
-      console.log('res', res);
       setProducts(res.data);
     });
   }, []);
 
-  return <TableComponent tableData={products} type="products" />;
+  return (
+    <TableComponent
+      currentPage={currentPage}
+      setCurrentPage={setCurrentPage}
+      tableData={products}
+      type="products"
+    />
+  );
 };
 
 export default ProductsScreen;
