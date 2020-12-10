b39836bf4ee86d21250ec629f29715dca6d07b2b
bogdan.kulynka <bogdan.kulynka@soft-innovation.ch>
2020-12-10 10:37:57 +0200

    IAP-9267

---
+++ src/screens/RecoveryPasswordScreen/RecoveryPassword.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import RecoveryPassword from './RecoveryPassword';
+import { Button } from '@material-ui/core';
+import { Formik } from 'formik';
+
+describe('email', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(<RecoveryPassword/>);
+  });
+
+  it('should have a <Formik />', () => {
+    expect(wrapper.find(Formik)).toHaveLength(1);
+  });
+
+  it('should have input for email', () => {
+      expect(wrapper.find('input[name="email"]').exists()).toBeTruthy();
+  });
+
+  it('should have button', () => {
+      expect(wrapper.find(Button)).toHaveLength(1);
+  });
+})
