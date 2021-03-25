8e600a8213e8d56c812419564dac7c7010a6b76e
Markian Tsybran <mtsybran@nexway.com>
2021-03-25 14:56:39 +0200

    some fixes & improvements

--- src/screens/PricesDetailsScreen/PricesDetailsScreen.jsx
+++ src/screens/PricesDetailsScreen/PricesDetailsScreen.jsx
@@ -1,7 +1,9 @@
 // ToDo: consider making a common layout for such type of settings screens + refactor
 import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory, useParams } from 'react-router-dom';
+
+import moment from 'moment';
 
 import {
   Box,
@@ -9,34 +11,27 @@ import {
   LinearProgress,
   Zoom,
   Button,
-  Tabs,
-  Tab,
   Typography,
   RadioGroup,
   Radio,
   FormControlLabel,
-  Switch,
 } from '@material-ui/core';
 
-import moment from 'moment';
-
-import {
-  generateData,
-  defaultShow,
-} from '../../services/useData/tableMarkups/campaignPrices';
+import FileCopyOutlinedIcon from '@material-ui/icons/FileCopyOutlined';
 
 import { SelectCustom } from '../../components/Inputs';
+import CustomCard from '../../components/utils/CustomCard';
+import CustomBreadcrumbs from '../../components/utils/CustomBreadcrumbs';
+import DateRangePicker from '../../components/utils/Modals/DateRangePicker';
 
 import {
   priceCurrency,
   countryOptions,
 } from '../../services/selectOptions/selectOptions';
 
-import api from '../../api';
 import { showNotification } from '../../redux/actions/HttpNotifications';
-import CustomCard from '../../components/utils/CustomCard';
-import TableComponent from '../../components/TableComponent';
-import DateRangePicker from '../../components/utils/Modals/DateRangePicker';
+
+import api from '../../api';
 import localization from '../../localization';
 
 import './pricesDetailsScreen.scss';
@@ -44,9 +39,13 @@ import './pricesDetailsScreen.scss';
 const PricesDetailsScreen = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
+  const history = useHistory();
   const [price, setPrice] = useState(null);
   const [curPrice, setCurPrice] = useState(null);
+  const [availProducts, setAvailProducts] = useState([]);
   const [hasChanges, setHasChanges] = useState(false);
+  const [validPeriod, setValidPeriod] = useState('between');
+  const nxState = useSelector(({ account: { nexwayState } }) => nexwayState);
 
   const handleChange = (e) => {
     e.persist();
@@ -55,12 +54,21 @@ const PricesDetailsScreen = () => {
   };
 
   const doSave = () => {
-    api.updatePriceById(id, curPrice).then(() => {
-      dispatch(
-        showNotification(localization.t('general.updatesHaveBeenSaved')),
-      );
-      setPrice(curPrice);
-    });
+    if (id === 'add') {
+      api
+        .addNewPrice(curPrice)
+        .then(() => {
+          dispatch(showNotification(localization.t('general.updatesHaveBeenSaved')));
+          history.push('/marketing/prices');
+        });
+    } else {
+      api
+        .updatePriceById(id, curPrice)
+        .then(() => {
+          dispatch(showNotification(localization.t('general.updatesHaveBeenSaved')));
+          setPrice(curPrice);
+        });
+    }
   };
 
   useEffect(() => {
@@ -70,14 +78,50 @@ const PricesDetailsScreen = () => {
   }, [curPrice, price]);
 
   useEffect(() => {
-    api.getPriceById(id).then(({ data }) => {
-      setPrice(data);
-      setCurPrice(data);
-    });
-  }, []);
+    if (validPeriod === 'after') {
+      const newPrice = { ...curPrice };
+      delete newPrice.endDate;
+      setCurPrice(newPrice);
+    }
+  }, [validPeriod]);
 
-  const updatePrice = (type, value) =>
-    setCurPrice((c) => ({ ...c, [type]: value }));
+  useEffect(() => {
+    const { selectedCustomer } = nxState;
+    
+    const initData = {
+      customerId: selectedCustomer?.id,
+      currency: '',
+      value: '',
+      msrp: '',
+      upSell: '',
+      crossSell: '',
+      vatIncluded: false,
+      startDate: new Date(),
+    };
+
+    if (id === 'add') {
+      if (selectedCustomer?.id) {
+        api
+          .getProducts(0, `&customerId=${selectedCustomer?.id}&status=ENABLED`)
+          .then(({ data: { items }}) => {
+            const products = items.map((it) => ({ id: it.id, value: it.genericName }));
+            
+            setAvailProducts(products);
+          });
+      }
+
+      setPrice({ ...initData });
+      setCurPrice({ ...initData });
+    } else {
+      api
+        .getPriceById(id)
+        .then(({ data }) => {
+          setPrice({ ...initData, ...data });
+          setCurPrice({ ...initData, ...data });
+          setValidPeriod(data.endDate ? 'between' : 'after');
+        });
+    }
+  }, []);
 
   const handleSelectDate = (ranges) => {
     const { startDate, endDate } = ranges;
@@ -89,23 +133,48 @@ const PricesDetailsScreen = () => {
   };
 
   const handleCountry = (e) => {
-    setCurPrice((c) => ({ ...c, country: e.target.value }));
+    if (e.target.value === 'default' && curPrice?.country) {
+      const newPrice = { ...curPrice };
+      delete newPrice.country;
+      
+      setCurPrice({ ...newPrice });
+    } else {
+      setCurPrice((c) => ({ ...c, country: e.target.value }));
+    }
   };
 
   const selectionRange = {
-    startDate: new Date(curPrice?.startDate),
-    endDate: new Date(curPrice?.endDate),
+    startDate: curPrice?.startDate ? new Date(curPrice?.startDate) : new Date(),
+    endDate: curPrice?.endDate ? new Date(curPrice?.endDate) : new Date(),
     key: 'selection',
   };
 
   if (curPrice === null) return <LinearProgress />;
 
+  if (id === 'add' && !curPrice?.customerId) return (
+    <Box textAlign='center'>
+      <Typography gutterBottom variant="h4">
+        {localization.t('general.noCustomer')}
+      </Typography>
+
+      <Typography gutterBottom variant="h5">
+        {localization.t('general.selectCustomer')}
+      </Typography>
+    </Box>
+  );
+
   return (
     <div className="price-details-screen">
-      <Tabs value={0} indicatorColor="primary" textColor="primary">
-        <Tab label={price.id} />
-      </Tabs>
+      <CustomBreadcrumbs
+        url='/marketing/prices'
+        section={localization.t('general.price')}
+        id={price?.id ? price.id : localization.t('general.addPrice')}
+      />
 
+      <Box my={2}>
+        <Typography gutterBottom variant="h3">{price?.customerId}</Typography>
+      </Box>
+      
       <Zoom in={hasChanges}>
         <Button
           id="save-price-button"
@@ -120,74 +189,87 @@ const PricesDetailsScreen = () => {
       </Zoom>
 
       <CustomCard title="General" style={{ marginTop: '20px' }}>
-        <Box mt={4} display="flex">
-          <Typography gutterBottom variant="h5" style={{ marginRight: '10px' }}>
-            Product ID
-          </Typography>
+        {
+          price?.productId ? (
+            <Box mt={4} display="flex">
+              <Typography gutterBottom variant="h5" style={{ marginRight: '10px' }}>
+                {localization.t('labels.productID')}
+              </Typography>
 
-          <Typography variant="h6">{price.productId}</Typography>
-        </Box>
+              <Typography variant="h6">{price?.productId}</Typography>
+
+              <FileCopyOutlinedIcon
+                style={{ opacity: .25, marginLeft: 10, cursor: 'pointer' }}
+                onClick={() => history.push(`/overview/products/${price?.productId}`)}
+              />
+            </Box>
+          ) : (
+            <Box width='50%' pr={4}>
+              <SelectCustom
+                label="productID"
+                value={curPrice?.productId}
+                isRequired
+                selectOptions={availProducts}
+                onChangeSelect={(e) => setCurPrice((c) => ({ ...c, productId: e.target.value }))}
+              />
+            </Box>
+          )
+        }
 
         <Box display="flex" mb={4}>
-          <Box width="50%" mr={4}>
-            <Box width="250px">
+          <Box width="50%" mr={4} display='flex'>
+            <Box minWidth="200px">
               <SelectCustom
-                label="priceCurrency"
+                label="currency"
+                isRequired
                 value={curPrice?.currency}
                 selectOptions={priceCurrency}
-                onChangeSelect={(e) =>
-                  setCurPrice((c) => ({ ...c, currency: e.target.value }))
-                }
+                onChangeSelect={(e) => setCurPrice((c) => ({ ...c, currency: e.target.value }))}
               />
             </Box>
 
-            <TextField
-              fullWidth
-              name="value"
-              label="Value"
-              type="text"
-              value={curPrice.value}
-              onChange={(e) =>
-                setCurPrice((c) => ({ ...c, value: e.target.value }))
-              }
-              variant="outlined"
-            />
+            <Box ml={4}>
+              <TextField
+                fullWidth
+                name="value"
+                label="Value"
+                type="text"
+                required
+                value={curPrice.value}
+                onChange={handleChange}
+                variant="outlined"
+              />
 
-            <TextField
-              fullWidth
-              name="msrp"
-              label="MSRP"
-              type="text"
-              value={curPrice.msrp}
-              onChange={(e) =>
-                setCurPrice((c) => ({ ...c, msrp: e.target.value }))
-              }
-              variant="outlined"
-            />
+              <TextField
+                fullWidth
+                name="msrp"
+                label="MSRP"
+                type="text"
+                value={curPrice.msrp}
+                onChange={handleChange}
+                variant="outlined"
+              />
 
-            <TextField
-              fullWidth
-              name="upsellPrice"
-              type="text"
-              label="Upsell price"
-              value={curPrice.upsellPrice}
-              onChange={(e) =>
-                setCurPrice((c) => ({ ...c, upsellPrice: e.target.value }))
-              }
-              variant="outlined"
-            />
+              <TextField
+                fullWidth
+                name="upSell"
+                type="text"
+                label="Upsell price"
+                value={curPrice.upSell}
+                onChange={handleChange}
+                variant="outlined"
+              />
 
-            <TextField
-              fullWidth
-              name="crossSellPrice"
-              type="text"
-              label="Cross-sell price"
-              value={curPrice.crossSellPrice}
-              onChange={(e) =>
-                setCurPrice((c) => ({ ...c, crossSellPrice: e.target.value }))
-              }
-              variant="outlined"
-            />
+              <TextField
+                fullWidth
+                name="crossSell"
+                type="text"
+                label="Cross-sell price"
+                value={curPrice.crossSell}
+                onChange={handleChange}
+                variant="outlined"
+              />
+            </Box>
           </Box>
 
           <Box width="50%" ml={4}>
@@ -206,26 +288,64 @@ const PricesDetailsScreen = () => {
               <RadioGroup
                 row
                 aria-label="vat"
-                name="VAT"
+                name="vatIncluded"
                 value={curPrice.vatIncluded ? 'YES' : 'NO'}
-                onChange={(e) =>
-                  setCurPrice((c) => ({
-                    ...c,
-                    vatIncluded: e.target.value === 'YES',
-                  }))
-                }
+                onChange={(e) => setCurPrice((c) => ({ ...c, vatIncluded: e.target.value === 'YES' }))}
               >
                 <FormControlLabel
                   value="YES"
                   control={<Radio color="primary" />}
                   label="YES"
                 />
+
                 <FormControlLabel
                   value="NO"
                   control={<Radio color="primary" />}
                   label="NO"
                 />
               </RadioGroup>
+            </Box>
+
+            <Box mt='8px'>
+              <Typography variant="h5">{localization.t('labels.periodOfValidity')} *</Typography>
+
+              <Box py={2} display='flex'>
+                <Box width='200px'>
+                  <SelectCustom
+                    label="periodOfValidity"
+                    onChangeSelect={(e) => setValidPeriod(e.target.value)}
+                    selectOptions={[
+                      { id: 'between', value: 'between' },
+                      { id: 'after', value: 'after' },
+                    ]}
+                    value={validPeriod}
+                  />
+                </Box>
+
+                <Box ml={4}>
+                  {validPeriod === 'between' ? (
+                    <Box display='flex' alignItems='center' mt='8px' height='100%'>
+                      <DateRangePicker
+                        values={selectionRange}
+                        handleChange={handleSelectDate}
+                      />
+                    </Box>
+                  ) : (
+                    <form noValidate>
+                      <TextField
+                        fullWidth
+                        name="startDate"
+                        value={curPrice.startDate ? moment(curPrice.startDate).format('YYYY-MM-DD') : ''}
+                        label={localization.t('labels.startDate')}
+                        type="date"
+                        variant="outlined"
+                        InputLabelProps={{ shrink: true }}
+                        onChange={handleChange}
+                      />
+                    </form>
+                  )}
+                </Box>
+              </Box>
             </Box>
           </Box>
         </Box>
