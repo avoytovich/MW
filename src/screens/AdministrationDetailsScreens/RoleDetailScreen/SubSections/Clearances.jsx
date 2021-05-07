5416d60b3862e131fd46318afc688ffae56ad976
Lyudmyla Pomohaibo <lyudmyla.pomohaibo@soft-innovation.ch>
2021-05-07 16:08:07 +0300

    IAP-10566/feature; Fix eslint errors;

--- src/screens/AdministrationDetailsScreens/RoleDetailScreen/SubSections/Clearances.jsx
+++ src/screens/AdministrationDetailsScreens/RoleDetailScreen/SubSections/Clearances.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {
   LinearProgress,
   Box,
-  Typography
+  Typography,
 } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import localization from '../../../../localization';
@@ -10,43 +10,43 @@ import { SelectWithChip } from '../../../../components/Inputs';
 import ClearancesInputs from './ClearancesInputs';
 
 const Clearances = ({ curRole, setCurRole, selectOptions }) => (
-  curRole === null ?
-    <LinearProgress /> :
-    <Box p={2}>
-      <ClearancesInputs
-        setCurRole={setCurRole}
-        curRole={curRole}
-        selectOptions={selectOptions}
-      />
-      <Box py={4}>
-        <Typography gutterBottom variant="h4">
-          {localization.t(`labels.conditionsOfAvailability`)}
-        </Typography>
-      </Box>
-      <Box pt={2}>
-        <SelectWithChip
-          label='conditionsOfAvailability'
-          value={curRole.availabilityConditions}
-          selectOptions={selectOptions.conditionsOfAvailability}
-          onChangeSelect={(e) =>
-            setCurRole({
+  curRole === null
+    ? <LinearProgress />
+    : (
+      <Box p={2}>
+        <ClearancesInputs
+          setCurRole={setCurRole}
+          curRole={curRole}
+          selectOptions={selectOptions}
+        />
+        <Box py={4}>
+          <Typography gutterBottom variant="h4">
+            {localization.t('labels.conditionsOfAvailability')}
+          </Typography>
+        </Box>
+        <Box pt={2}>
+          <SelectWithChip
+            label='conditionsOfAvailability'
+            value={curRole.availabilityConditions}
+            selectOptions={selectOptions.conditionsOfAvailability}
+            onChangeSelect={(e) => setCurRole({
               ...curRole,
               availabilityConditions: e.target.value,
-            })
-          }
-          onClickDelIcon={(chip) => {
-            const newValue = [...curRole.availabilityConditions].filter(
-              (val) => val !== chip,
-            );
-            setCurRole({
-              ...curRole,
-              availabilityConditions: newValue,
-            });
-          }}
-        />
+            })}
+            onClickDelIcon={(chip) => {
+              const newValue = [...curRole.availabilityConditions].filter(
+                (val) => val !== chip,
+              );
+              setCurRole({
+                ...curRole,
+                availabilityConditions: newValue,
+              });
+            }}
+          />
+        </Box>
       </Box>
-    </Box>
-)
+    )
+);
 
 Clearances.propTypes = {
   curRole: PropTypes.object,
