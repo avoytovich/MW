017359d849c3c51fa8a8b5d1cfbf75d45ed1995d
Markian Tsybran <mtsybran@nexway.com>
2021-05-28 17:59:03 +0300

    revert

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
-    publicPath: '/iap-10616/',
+    publicPath: '/',
   },
   devServer: {
     historyApiFallback: true,
