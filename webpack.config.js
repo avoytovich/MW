2907f8d670fa0a1d3ce68de2933fe53ffb7fbb7e
Markian Tsybran <mtsybran@nexway.com>
2021-06-14 13:26:26 +0300

    hotfix

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = (env) => ({
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
-    publicPath: env.branch ? `/${env.branch.toLowerCase()}/` : '/',
+    publicPath: env.branch && env.branch !== 'master' ? `/${env.branch.toLowerCase()}/` : '/',
   },
   devServer: {
     historyApiFallback: true,
