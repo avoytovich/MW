7a3fdaa52005b2a0ac999c58be78295160de228b
Andriy Voytovych <avoytovych@nexway.com>
2022-07-20 13:08:18 +0000

    IAP-14137

---
+++ src/screens/DiscountsScreen/DiscountsScreen.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
+
+
+import DiscountsScreen from './DiscountsScreen';
+
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useDispatch: jest.fn().mockImplementation(() => jest.fn()),
+  useSelector: jest.fn().mockImplementation(() => []),
+}));
+
+describe('<DiscountsScreen />', () => {
+  let wrapper;
+  const setState = jest.fn();
+  const useStateSpy = jest.spyOn(React, 'useState');
+  useStateSpy.mockImplementation((init) => [init, setState]);
+
+  beforeEach(() => {
+    wrapper = mount(
+      <BrowserRouter>
+        <DiscountsScreen />
+      </BrowserRouter>
+    );
+  });
+
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(wrapper)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+});
