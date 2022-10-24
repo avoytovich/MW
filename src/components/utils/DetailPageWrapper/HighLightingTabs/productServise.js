7a4b3b1c7e0071fdd93f4fa9ba6dfaea8993e518
Andriy Voytovych <avoytovych@nexway.com>
2022-10-24 19:20:57 +0000

    IAP-14733

--- src/components/utils/DetailPageWrapper/HighLightingTabs/productServise.js
+++ src/components/utils/DetailPageWrapper/HighLightingTabs/productServise.js
@@ -29,68 +29,64 @@ const productHightLight = (
   tabs,
   localizedErrors = {},
 ) => {
-  if (!Object.keys(errors).length) {
-    if (tabs?.tabLabels?.[tabs.curTab] === 'general') {
-      setErrors({
-        ...errors,
-        [tabs?.tabLabels?.[tabs.curTab]]: {
-          ...errors?.[tabs?.tabLabels?.[tabs.curTab]],
-          isFulfilled: hightLightGeneral(curData),
-        },
-      });
-    }
-  } else if (tabs?.tabLabels?.[tabs.curTab] !== 'general') {
-    if (tabs?.tabLabels?.[tabs.curTab] === 'prices') {
-      setErrors({
-        ...errors,
-        general: {
-          isFulfilled: hightLightGeneral(curData),
-          name: errors?.general?.name ? true : !curData.genericName,
-          type: errors?.general?.type ? true : !curData.type,
-          publisherRefId: errors?.general?.publisherRefId ? true : !curData.publisherRefId,
-        },
-        [tabs?.tabLabels?.[tabs.curTab]]: {
-          ...errors?.[tabs?.tabLabels?.[tabs.curTab]],
-          isFulfilledParent: (!curData.parentId && !curData.prices.value)
-            ? (curData.prices.state === 'inherits' ? null : hightLightPrices(curData, priceTableError)) : null,
-          isFulfilledVariant: (curData.parentId || curData.prices.value)
-            ? (curData.prices.state === 'inherits' ? null : hightLightPrices(curData, priceTableError)) : null,
-          currencyParent: (errors?.prices?.currency && !curData?.parentId)
+  if (tabs?.tabLabels?.[tabs.curTab] === 'general') {
+    setErrors({
+      ...errors,
+      [tabs?.tabLabels?.[tabs.curTab]]: {
+        ...errors?.[tabs?.tabLabels?.[tabs.curTab]],
+        isFulfilled: hightLightGeneral(curData),
+      },
+    });
+  } else if (tabs?.tabLabels?.[tabs.curTab] === 'prices') {
+    setErrors({
+      ...errors,
+      general: {
+        isFulfilled: hightLightGeneral(curData),
+        name: errors?.general?.name ? true : !curData.genericName,
+        type: errors?.general?.type ? true : !curData.type,
+        publisherRefId: errors?.general?.publisherRefId ? true : !curData.publisherRefId,
+      },
+      [tabs?.tabLabels?.[tabs.curTab]]: {
+        ...errors?.[tabs?.tabLabels?.[tabs.curTab]],
+        isFulfilledParent: (!curData.parentId && !curData.prices.value)
+          ? (curData.prices.state === 'inherits' ? null : hightLightPrices(curData, priceTableError)) : null,
+        isFulfilledVariant: (curData.parentId || curData.prices.value)
+          ? (curData.prices.state === 'inherits' ? null : hightLightPrices(curData, priceTableError)) : null,
+        currencyParent: (errors?.prices?.currency && !curData?.parentId)
             || curData?.prices?.defaultCurrency
-            ? false : !Object.keys(curData?.prices?.priceByCountryByCurrency || []).length,
-          currencyVariant: errors?.prices?.currency
+          ? false : !Object.keys(curData?.prices?.priceByCountryByCurrency || []).length,
+        currencyVariant: errors?.prices?.currency
             || curData?.prices?.value?.defaultCurrency
-            ? false : !Object.keys(curData?.prices?.value?.priceByCountryByCurrency || []).length,
-          price: errors?.prices?.price ? true : !!priceTableError.length,
-        },
-      });
-    } else if (tabs?.tabLabels?.[tabs.curTab] === 'localizedContent') {
-      setErrors({
-        ...errors,
-        general: {
-          isFulfilled: hightLightGeneral(curData),
-          name: errors?.general?.name ? true : !curData.genericName,
-          type: errors?.general?.type ? true : !curData.type,
-          publisherRefId: errors?.general?.publisherRefId ? true : !curData.publisherRefId,
-        },
-        [tabs?.tabLabels?.[tabs.curTab]]: {
-          ...errors?.[tabs?.tabLabels?.[tabs.curTab]],
-          isFulfilled: hightLightLocalizedContent(localizedErrors, curLocalizedData),
-          defaultLanguage: curLocalizedData?.fallbackLocale === '' || curLocalizedData?.fallbackLocale?.value === '',
-          marketingName: !!Object.keys(localizedErrors).length,
-        },
-      });
-    } else {
-      setErrors({
-        ...errors,
-        general: {
-          isFulfilled: hightLightGeneral(curData),
-          name: errors?.general?.name ? true : !curData.genericName,
-          type: errors?.general?.type ? true : !curData.type,
-          publisherRefId: errors?.general?.publisherRefId ? true : !curData.publisherRefId,
-        },
-      });
-    }
+          ? false : !Object.keys(curData?.prices?.value?.priceByCountryByCurrency || []).length,
+        price: errors?.prices?.price ? true : !!priceTableError.length,
+      },
+    });
+  } else if (tabs?.tabLabels?.[tabs.curTab] === 'localizedContent') {
+    setErrors({
+      ...errors,
+      general: {
+        isFulfilled: hightLightGeneral(curData),
+        name: errors?.general?.name ? true : !curData.genericName,
+        type: errors?.general?.type ? true : !curData.type,
+        publisherRefId: errors?.general?.publisherRefId ? true : !curData.publisherRefId,
+      },
+      [tabs?.tabLabels?.[tabs.curTab]]: {
+        ...errors?.[tabs?.tabLabels?.[tabs.curTab]],
+        isFulfilled: hightLightLocalizedContent(localizedErrors, curLocalizedData),
+        defaultLanguage: curLocalizedData?.fallbackLocale === '' || curLocalizedData?.fallbackLocale?.value === '',
+        marketingName: !!Object.keys(localizedErrors).length,
+      },
+    });
+  } else {
+    setErrors({
+      ...errors,
+      general: {
+        isFulfilled: hightLightGeneral(curData),
+        name: errors?.general?.name ? true : !curData.genericName,
+        type: errors?.general?.type ? true : !curData.type,
+        publisherRefId: errors?.general?.publisherRefId ? true : !curData.publisherRefId,
+      },
+    });
   }
 };
 
